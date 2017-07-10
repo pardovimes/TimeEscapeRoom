eec6904fa6fc228a51d9ec5ea5124f8359972a26
pardtheman@gmail.com <pardtheman@gmail.com>
2017-07-10 19:06:31 +0200

    Deploying project

---
+++ TimeEscapeRoom/Source/TimeEscapeRoom/TimeEscapeRoom.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TimeEscapeRoom.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimeEscapeRoom, "TimeEscapeRoom" );
---
+++ TimeEscapeRoom/Source/TimeEscapeRoom/TimeEscapeRoomGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TimeEscapeRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMEESCAPEROOM_API ATimeEscapeRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
